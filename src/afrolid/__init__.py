aa7b96b91fd48335ef7d53a9b14a1e66f295ead4
theyorubayesian <akin.o.oladipo@gmail.com>
2024-11-09 02:10:27 +0000

    add prediction utils

--- src/afrolid/__init__.py
+++ src/afrolid/__init__.py
@@ -1,6 +1,16 @@
 from .conversion import create_pytorch_state_dict
+from .language_info import LanguageInfo, Languages
 from .model import AfroLIDModel
-from .utils import download_and_extract_model, load_afrolid_artifacts
+from .utils import download_and_extract_model, load_afrolid_artifacts, prepare_inputs_for_model, predict_language
 
 
-__all__ = [AfroLIDModel, create_pytorch_state_dict, download_and_extract_model, load_afrolid_artifacts]
+__all__ = [
+    AfroLIDModel,
+    create_pytorch_state_dict,
+    download_and_extract_model,
+    LanguageInfo,
+    Languages,
+    load_afrolid_artifacts,
+    prepare_inputs_for_model,
+    predict_language
+]
--- src/afrolid/utils.py
+++ src/afrolid/utils.py
@@ -4,14 +4,14 @@ import tarfile
 import tempfile
 from pathlib import Path
 from platformdirs import user_cache_dir
-from typing import Final, Optional
+from typing import Any, Final, Optional
 
-import sentencepiece as spm
 import torch
 from tqdm import tqdm
+from transformers import BatchEncoding, T5Tokenizer
 
 from .conversion import create_pytorch_state_dict
-from .language_info import LanguageInfo
+from .language_info import LanguageInfo, Languages
 from .model import AfroLIDModel
 
 AFROLID_CACHE_DIR: Final[Path] = Path(os.getenv("AFROLID_CACHE_DIR", user_cache_dir('afrolid')))
@@ -46,7 +46,7 @@ def download_and_extract_model(path: Optional[str] = None) -> None:
         download_path.unlink()
 
 
-def load_afrolid_artifacts(download_path: Optional[str] = None) -> tuple[AfroLIDModel, spm.SentencePieceProcessor, LanguageInfo]:
+def load_afrolid_artifacts(download_path: Optional[str] = None) -> tuple[AfroLIDModel, T5Tokenizer, LanguageInfo]:
     afrolid = AfroLIDModel()
 
     model_path = Path(download_path) if download_path else AFROLID_CACHE_DIR
@@ -64,13 +64,48 @@ def load_afrolid_artifacts(download_path: Optional[str] = None) -> tuple[AfroLID
 
         afrolid.load_state_dict(torch_dict, strict=False)
 
-    tokenizer = spm.SentencePieceProcessor()
-    tokenizer.Load(str(model_path / "afrolid_model/afrolid_spm_517_bpe.model"))
-
+    afrolid = afrolid.eval()
+    
+    tokenizer = T5Tokenizer.from_pretrained(str(model_path / "afrolid_model/afrolid_spm_517_bpe.model"))
+    tokenizer.pad_token_id = 1
+    tokenizer.eos_token_id = 2
+    tokenizer.unk_token_id = 3
+    tokenizer.model_max_length = 1024
     language_info = LanguageInfo(model_path / "afrolid_model/dict.label.txt")
 
     return afrolid, tokenizer, language_info
 
 
-def prepare_input_for_model(text: str, tokenizer: spm.SentencePieceProcessor) -> torch.Tensor:
-    return torch.IntTensor([x+1 for x in tokenizer.EncodeAsIds(text)] + [2])
+def prepare_inputs_for_model(text: str | list[str], tokenizer: T5Tokenizer, **encoding_kwargs: Any) -> BatchEncoding:
+    encoding = tokenizer(text, return_tensors="pt", padding=True, truncation=True, **encoding_kwargs)
+
+    not_eos_mask = (encoding["input_ids"] != tokenizer.eos_token_id)
+    combined_mask = encoding["attention_mask"] * not_eos_mask
+
+    # Increment only the non-masked and non-EOS positions by 1
+    # FairSeq's encodings always returns 1 greater than the spm Processor's encoding
+    encoding["input_ids"] = encoding["input_ids"] + combined_mask
+    return encoding
+
+
+def predict_language(
+    text: str | list[str],
+    model: AfroLIDModel,
+    tokenizer: T5Tokenizer,
+    languages: Languages,
+    top_k: int = 3,
+    **tokenizer_kwargs
+) -> list[list[LanguageInfo]]:
+    encoding = prepare_inputs_for_model(text, tokenizer, **tokenizer_kwargs)
+    outputs = model(encoding["input_ids"]).detach().topk(top_k)
+
+    probabilities = outputs.values.squeeze().tolist()
+    language_ids = outputs.indices.squeeze().tolist()
+
+    languages = [[languages[_id] for _id in id_list] for id_list in language_ids]
+
+    for idx, predicted_languages in enumerate(languages):
+        for lang_idx, info in enumerate(predicted_languages):
+            info["probability"] = probabilities[idx][lang_idx]
+    
+    return languages
